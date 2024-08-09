fc554795eea1f3d4c059d0b8ead152c008a5de3b
richard1927 <54837611+richard1927@users.noreply.github.com>
2024-08-09 09:32:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
+   print('Request for index page received!')
    return render_template('index.html')
 
 @app.route('/favicon.ico')
